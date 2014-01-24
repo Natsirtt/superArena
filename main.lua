b3d19ea42aff7c50fe7c224a0fe6cbbb32177807
Tristan Louet <natsirtt@gmail.com>
2014-01-24 20:23:23 +0100

    Basic love2d window

--- main.lua
+++ main.lua
@@ -1,10 +1,15 @@
-
 function love.load(arg)
-
+    love.window.setTitle("Bugfree Happiness")
+    love.window.setMode(1024, 768, {
+        fullscreen = false,
+        fsaa = 4,
+    })
 end
 
 function love.update(dt)
- 
+    if (love.keyboard.isDown("escape")) then
+        love.event.quit()
+    end
 end
 
 function love.draw()
