3aabd9e21a2f2f94b27e07837fad17b042b72fc8
Tristan Louet <tristan.louet.dev@gmail.com>
2014-03-03 01:56:23 +0100

    All buttons set on the Windows pattern

--- controllers/gamepadController.lua
+++ controllers/gamepadController.lua
@@ -1,16 +1,91 @@
 local mt = {}
 mt.__index = mt
 
-local buttons = {}
+-- global array (to use like a buttons enum)
+buttons = {
+    A = 0,
+    B = 1,
+    X = 2,
+    Y = 3,
+    UP = 4,
+    DOWN = 5,
+    LEFT = 6,
+    RIGHT = 7,
+    JOYSTICK_LEFT = 8,
+    JOYSTICK_RIGHT = 9,
+    LB = 10,
+    RB = 11,
+    START = 14,
+    SELECT = 15
+}
 
-buttons["Windows"] = {}
-buttons["Linux"] = {}
-buttons["OS X"] = {}
-buttons["Android"] = {}
+local buttonsMap = {}
+
+buttonsMap["Windows"] = {}
+buttonsMap["Linux"] = {}
+buttonsMap["OS X"] = {}
+buttonsMap["Android"] = {}
 
 -- Initializing all arrays manually to have cross-platforme coherence in inputs
+buttonsMap["Windows"][buttons.A] = 10
+buttonsMap["Windows"][buttons.B] = 11
+buttonsMap["Windows"][buttons.X] = 12
+buttonsMap["Windows"][buttons.Y] = 13
+buttonsMap["Windows"][buttons.JOYSTICK_LEFT] = 6
+buttonsMap["Windows"][buttons.JOYSTICK_RIGHT] = 7
+buttonsMap["Windows"][buttons.UP] = 0
+buttonsMap["Windows"][buttons.DOWN] = 1
+buttonsMap["Windows"][buttons.LEFT] = 2
+buttonsMap["Windows"][buttons.RIGHT] = 3
+buttonsMap["Windows"][buttons.START] = 4
+buttonsMap["Windows"][buttons.SELECT] = 5
+buttonsMap["Windows"][buttons.LB] = 8
+buttonsMap["Windows"][buttons.RB] = 9
 
---TODO
+buttonsMap["Linux"][buttons.A] = 10
+buttonsMap["Linux"][buttons.B] = 11
+buttonsMap["Linux"][buttons.X] = 12
+buttonsMap["Linux"][buttons.Y] = 13
+buttonsMap["Linux"][buttons.JOYSTICK_LEFT] = 6
+buttonsMap["Linux"][buttons.JOYSTICK_RIGHT] = 7
+buttonsMap["Linux"][buttons.UP] = 0
+buttonsMap["Linux"][buttons.DOWN] = 1
+buttonsMap["Linux"][buttons.LEFT] = 2
+buttonsMap["Linux"][buttons.RIGHT] = 3
+buttonsMap["Linux"][buttons.START] = 4
+buttonsMap["Linux"][buttons.SELECT] = 5
+buttonsMap["Linux"][buttons.LB] = 8
+buttonsMap["Linux"][buttons.RB] = 9
+
+buttonsMap["OS X"][buttons.A] = 10
+buttonsMap["OS X"][buttons.B] = 11
+buttonsMap["OS X"][buttons.X] = 12
+buttonsMap["OS X"][buttons.Y] = 13
+buttonsMap["OS X"][buttons.JOYSTICK_LEFT] = 6
+buttonsMap["OS X"][buttons.JOYSTICK_RIGHT] = 7
+buttonsMap["OS X"][buttons.UP] = 0
+buttonsMap["OS X"][buttons.DOWN] = 1
+buttonsMap["OS X"][buttons.LEFT] = 2
+buttonsMap["OS X"][buttons.RIGHT] = 3
+buttonsMap["OS X"][buttons.START] = 4
+buttonsMap["OS X"][buttons.SELECT] = 5
+buttonsMap["OS X"][buttons.LB] = 8
+buttonsMap["OS X"][buttons.RB] = 9
+
+buttonsMap["Android"][buttons.A] = 10
+buttonsMap["Android"][buttons.B] = 11
+buttonsMap["Android"][buttons.X] = 12
+buttonsMap["Android"][buttons.Y] = 13
+buttonsMap["Android"][buttons.JOYSTICK_LEFT] = 6
+buttonsMap["Android"][buttons.JOYSTICK_RIGHT] = 7
+buttonsMap["Android"][buttons.UP] = 0
+buttonsMap["Android"][buttons.DOWN] = 1
+buttonsMap["Android"][buttons.LEFT] = 2
+buttonsMap["Android"][buttons.RIGHT] = 3
+buttonsMap["Android"][buttons.START] = 4
+buttonsMap["Android"][buttons.SELECT] = 5
+buttonsMap["Android"][buttons.LB] = 8
+buttonsMap["Android"][buttons.RB] = 9
 
 function newGamepadController(joystick)
     local this = {}
@@ -118,5 +193,10 @@ function mt:update(dt)
 end
 
 function mt:draw()
-
+    for i = 0, 20 do
+        if self:isDown(i) then
+            love.graphics.print(i, 20, 20)
+            break
+        end
+    end
 end
